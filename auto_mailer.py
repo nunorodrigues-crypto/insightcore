00d671817b244c6068f33839e620ad7dbf7d98ac
nuno.rodrigures-insightkube <nuno.rodrigues@insightkube.com>
2026-04-07 12:31:37 +0100

    Update auto_mailer.py

--- auto_mailer.py
+++ auto_mailer.py
@@ -82,7 +82,7 @@ def enviar_relatorio():
             </div>
 
             <div style="text-align: center;">
-                <a href="https://insightcore.streamlit.app" 
+                <a href="https://insightcore-fdctr3i6ssvwyxwdzilc7b.streamlit.app" 
                    style="background-color: #2c3e50; color: white; padding: 15px 30px; text-decoration: none; border-radius: 6px; font-weight: bold; display: inline-block; font-size: 15px;">
                    Abrir Painel Completo na Plataforma →
                 </a>
